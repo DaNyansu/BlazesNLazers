5926d692cffa94950eef077a63152258220907c6
Havukainen Anniina <Anniina.Havukainen@joutsenonopisto.fi>
2018-04-16 09:55:01 +0300

    Enemy generation fixed

--- Assets/Scripts/objectPooler.cs
+++ Assets/Scripts/objectPooler.cs
@@ -2,28 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class objectPooler : MonoBehaviour {
+public class objectPooler : MonoBehaviour
+{
 
     public GameObject pooledObject;
+
+    public GameObject[] pooledArrayObject = new GameObject[5];
 
     public int pooledAmount;
 
     List<GameObject> pooledObjects;
-    
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start()
+    {
         pooledObjects = new List<GameObject>();
 
-        for(int i = 0; i < pooledAmount; i++)
+        if (gameObject.name == "EnemyPool")
         {
-            GameObject obj = (GameObject)Instantiate(pooledObject);
-            obj.name = pooledObject.name + i;
-            obj.SetActive(false);
-            pooledObjects.Add(obj);
+            for (int i = 0; i < pooledAmount; i++)
+            {
+                for(int p = 0; p < pooledArrayObject.Length; p++)
+                {
+                    GameObject obj = (GameObject)Instantiate(pooledArrayObject[p]);
+                    obj.name = pooledArrayObject[p].name + i;
+                    obj.SetActive(false);
+                    pooledObjects.Add(obj);
+                }
+                
+            }
         }
-		
-	}
+
+        else
+        {
+            for (int i = 0; i < pooledAmount; i++)
+            {
+                GameObject obj = (GameObject)Instantiate(pooledObject);
+                obj.name = pooledObject.name + i;
+                obj.SetActive(false);
+                pooledObjects.Add(obj);
+            }
+        }
+    }
 
     private void Update()
     {
@@ -40,9 +61,9 @@ public class objectPooler : MonoBehaviour {
 
     public GameObject GetPooledObject()
     {
-        for(int i = 0; i < pooledObjects.Count; i++)
+        for (int i = 0; i < pooledObjects.Count; i++)
         {
-            if(!pooledObjects[i].activeInHierarchy)
+            if (!pooledObjects[i].activeInHierarchy)
             {
                 return pooledObjects[i];
             }
